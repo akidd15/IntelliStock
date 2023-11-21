707772e7efe50476b53099cb468ab6f44730be33
Jake Preciado <jacobpreciado@gmail.com>
2023-11-20 19:24:46 -0700

    Landing page structure built

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,7 +1,45 @@
+import { useState } from 'react';
+
 export default function Landing() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('User: ' + username);
+        console.log('Password: ' + password);
+    }
+
     return (
-        <form>
-            
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={handleUsername}
+                required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePassword}
+                required
+            />
+
+            <button type="submit">Login</button>
         </form>
     )
 }
