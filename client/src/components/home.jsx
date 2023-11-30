f54380217fd06feee5aa8bf60b4e99d0801e8618
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-11-29 17:26:23 -0700

    data is added to home and categories

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,21 +1,50 @@
-import React from "react";
+
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+// added for database adding and query
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { ADD_CATEGORY } from "../utils/mutations";
+import { QUERY_CATEGORIES } from "../utils/queries";
+import Auth from '../utils/auth'
+
 const Home = () => {
     // state to hold list item
-    const [category, setCategory] = useState([]);
+    // const [category, setCategory] = useState([]);
     const [newCategory, setNewCategory] = useState('');
     const [popUp, setPopUp] = useState(false);
+    // added for database
+    const [addCategory, { error }] = useMutation(ADD_CATEGORY);
+    const { loading, data } = useQuery(QUERY_CATEGORIES);
+    const categories = data?.categories || [];
 
-    const handleAddCategory = () => {
-        if (newCategory.trim() !== '') {
-            setCategory([ ...category, newCategory]);
+    // added async (event)
+    const handleAddCategory = async (event) => {
+        // added event.preventDefault()
+        event.preventDefault();
+
+        try {
+            const { data } = await addCategory({
+                variables: {
+                    categoryName: newCategory,
+                    categoryAuthor: Auth.getProfile().data.username,
+                },
+            });
+
             setNewCategory('');
-            // open the pop up to notify the user "Category added successfully!"
-            setPopUp(true);
-
+            setPopUp(true)
+        } catch (err) {
+            console.error(err);
         }
+
+        // if (newCategory.trim() !== '') {
+        //     setCategory([ ...category, newCategory]);
+        //     setNewCategory('');
+        //     // open the pop up to notify the user "Category added successfully!"
+        //     setPopUp(true);
+
+        // }
     };
 
     const closePopUp = () => {
@@ -28,9 +57,9 @@ const Home = () => {
             <h1>Welcome!</h1>
             <h3>My Categories</h3>
         <ul>
-            {category.map((category, index) => (
+            {categories.map((category, index) => (
                 <li key={index}>
-                    <Link to={`/categories/${index}`}>{category}</Link>
+                    <Link to={`/categories/${index}`}>{category.categoryName}</Link>
                 </li>
             ))}
         </ul>
