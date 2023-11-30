ec9d468a097405c961fb2784c2bd2a6739a0d55b
Alissa <pattersonal10@gmail.com>
2023-11-29 22:57:07 -0600

    updated logic in loginform.jsx to directed auth user to home page

--- client/src/components/loginform.jsx
+++ client/src/components/loginform.jsx
@@ -1,61 +1,141 @@
 
-import { useState } from 'react';
+// import { useState } from 'react';
+// import { Form, Segment, Button } from 'semantic-ui-react';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../utils/mutations';
+
+
+// import Auth from '../utils/auth';
+
+//  export const LoginForm = (props) => {
+//   // removed username: ''
+//   const [formState, setFormState] = useState({ email: '' , password: '' });
+//   // commented out
+//   // const [username, setUsername] = useState('');
+//   // const [password, setPassword] = useState('');
+//   const [login, { error, data }] = useMutation(LOGIN_USER);
+
+//   // update state based on form input changes
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+      
+//     });
+//   };
+
+//   // submit form
+//   const handleLoginSubmit = async (event) => {
+//     console.log("submit")
+//     event.preventDefault();
+//     console.log(formState);
+//     try {
+//       const { data } = await login({
+//         variables: {
+//           ...formState
+//           //changed to above
+//           // email: formState.email,
+//           // password: password,
+//           // username: username, 
+//         },
+//       });
+
+//       Auth.login(data.login.token);
+//     } catch (e) {
+//       console.error(e);
+//     }
+
+//     // clear form values
+//     setFormState({
+//       email: '',
+//       // username: '',
+//       password: '',
+//     });
+//   };
+
+//   //export const LoginForm = ({ username, password, handleUsername, handlePassword, handleLoginSubmit }) => (
+//   return (
+//     <Form size="large" onSubmit={handleLoginSubmit}>
+//       <Segment stacked>
+//         <Form.Input
+//           fluid
+//           icon={null}
+//           placeholder="Email"
+//           name="email"
+//           type="email"
+//           value={
+//             formState.email
+//             // setUsername.username
+//           }
+//           onChange={handleChange}
+//           required
+//           style={{ fontSize: '1.2em', marginBottom: '1em' }}
+//         />
+
+//         <Form.Input
+//           fluid
+//           icon={null}
+//           placeholder="Password"
+//           name="password"
+//           type="password"
+//           value={
+//             formState.password
+//             // setPassword.password
+//           }
+//           onChange={handleChange}
+//           required
+//           style={{ fontSize: '1.2em', marginBottom: '1em' }}
+//         />
+
+//         <Button color="green" fluid size="large" type="submit" style={{ fontSize: '1.2em', marginBottom: '1em' }}>
+//           Login
+//         </Button>
+//       </Segment>
+//     </Form>
+//   );
+// }
+
+import React, { useState } from 'react';
 import { Form, Segment, Button } from 'semantic-ui-react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
-
 import Auth from '../utils/auth';
 
- export const LoginForm = (props) => {
-  // removed username: ''
-  const [formState, setFormState] = useState({ email: '' , password: '' });
-  // commented out
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
+export const LoginForm = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormState({
       ...formState,
       [name]: value,
-      
     });
   };
 
-  // submit form
   const handleLoginSubmit = async (event) => {
-    console.log("submit")
     event.preventDefault();
-    console.log(formState);
     try {
       const { data } = await login({
-        variables: {
-          ...formState
-          //changed to above
-          // email: formState.email,
-          // password: password,
-          // username: username, 
-        },
+        variables: { ...formState },
       });
 
       Auth.login(data.login.token);
+
+      window.location.replace('/home');
     } catch (e) {
       console.error(e);
     }
 
-    // clear form values
     setFormState({
       email: '',
-      // username: '',
       password: '',
     });
   };
 
-  //export const LoginForm = ({ username, password, handleUsername, handlePassword, handleLoginSubmit }) => (
   return (
     <Form size="large" onSubmit={handleLoginSubmit}>
       <Segment stacked>
@@ -65,10 +145,7 @@ import Auth from '../utils/auth';
           placeholder="Email"
           name="email"
           type="email"
-          value={
-            formState.email
-            // setUsername.username
-          }
+          value={formState.email}
           onChange={handleChange}
           required
           style={{ fontSize: '1.2em', marginBottom: '1em' }}
@@ -80,19 +157,22 @@ import Auth from '../utils/auth';
           placeholder="Password"
           name="password"
           type="password"
-          value={
-            formState.password
-            // setPassword.password
-          }
+          value={formState.password}
           onChange={handleChange}
           required
           style={{ fontSize: '1.2em', marginBottom: '1em' }}
         />
 
-        <Button color="green" fluid size="large" type="submit" style={{ fontSize: '1.2em', marginBottom: '1em' }}>
+        <Button
+          color="green"
+          fluid
+          size="large"
+          type="submit"
+          style={{ fontSize: '1.2em', marginBottom: '1em' }}
+        >
           Login
         </Button>
       </Segment>
     </Form>
   );
-}
+};
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,49 @@
+// const jwt = require('jsonwebtoken');
+// const { GraphQLError } = require('graphql');
+
+// // set token secret and expiration date
+// const secret = 'mysecretsshhhhh';
+// const expiration = '2h';
+
+// module.exports = {
+//   AuthenticationError: new GraphQLError('Could not authenticate user.', {
+//     extensions: {
+//       code: 'UNAUTHENTICATED',
+//     },
+//   }),
+//   // function for our authenticated routes
+//   authMiddleware: function (req, res, next) {
+//     // allows token to be sent via  req.query or headers
+//     let token = req.query.token || req.headers.authorization;
+
+//     // ["Bearer", "<tokenvalue>"]
+//     if (req.headers.authorization) {
+//       token = token.split(' ').pop().trim();
+//     }
+
+//     if (!token) {
+//       return res.status(400).json({ message: 'You have no token!' });
+//     }
+
+//     // verify token and get user data out of it
+//     try {
+//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//       req.user = data;
+//     } catch {
+//       console.log('Invalid token');
+//       return res.status(400).json({ message: 'invalid token!' });
+//     }
+
+//     // send to next endpoint
+//     next();
+//   },
+//   signToken: function ({ username, email, _id }) {
+//     const payload = { username, email, _id };
+
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
+
 const jwt = require('jsonwebtoken');
 const { GraphQLError } = require('graphql');
 
@@ -11,9 +57,9 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
-  // function for our authenticated routes
+
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+    // allows token to be sent via req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -29,17 +75,32 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    } catch (err) {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
     // send to next endpoint
     next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  loggedIn: function (token) {
+    // If there is a token and it's not expired, return `true`
+    return token && !this.isTokenExpired(token) ? true : false;
+  },
+
+  isTokenExpired: function (token) {
+    try {
+      const decoded = jwt.verify(token, secret);
+      return decoded.exp < Date.now() / 1000;
+    } catch (err) {
+      return true;
+    }
   },
 };
