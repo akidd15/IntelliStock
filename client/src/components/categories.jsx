5b7acce240fe586eb2b0de51e5acaf9f636c0798
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-12-03 02:45:49 -0700

    redesigned newOrder.jsx and categories.jsx

--- client/src/components/categories.jsx
+++ client/src/components/categories.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import Receipt from './receipt';
 import NewOrder from './newOrder';
+import AddItem from './addItem';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_SINGLE_CATEGORY } from '../utils/queries';
-import AddItem from './addItem';
 import { REMOVE_ITEM } from '../utils/mutations';
 import { 
     Icon, 
@@ -18,23 +18,33 @@ import {
     TableHeaderCell } from 'semantic-ui-react'
 
 export default function Categories() {
+    // variables for controlling Receipt modal
     const [modalOpenReceipt, setModalOpenReceipt] = useState(false);
+    // variables for controlling NewOrder modal
     const [modalOpenNewOrder, setModalOpenNewOrder] = useState(false);
+    // variables for controlling AddItem modal
     const [modalOpenAddItem, setModalOpenAddItem] = useState(false);
+    // Delete Item Mutation stored in removeItem
     const [removeItem] = useMutation(REMOVE_ITEM,{
         refetchQueries: [
             QUERY_SINGLE_CATEGORY
         ]
     });
-
+    // grabs the categoryId from the route /:categoryId
     const { categoryId } = useParams();
-
+    // this is how we have data to use in the table
     const { data } = useQuery(QUERY_SINGLE_CATEGORY, {
         variables: { categoryId: categoryId },
     });
-    const category = data?.category || [];
-    const items = category.items || [];
+    // possible variable being passed to NewOrder
+    const [currentData, setCurrentData]=useState('')
 
+    // check if there is data and if there is store it in category
+    // category is necessary to grab information
+    const category = data?.category || [];
+    // store one level down in category to a variable called items
+    const items = category.items || [];
+    // receives 'itemId' and uses it to call removeItem which is a delete mutation
     const handleDelete = async (itemId) => {
         
         try {
@@ -47,65 +57,73 @@ export default function Categories() {
             console.error('Error deleting item:', error.message);
         }
     };
-
+    // handles open modal for AddItem
     function openModalAddItem() {
         setModalOpenAddItem(true);
     }
-
+    // handles close modal for AddItem
     function closeModalAddItem() {
         setModalOpenAddItem(false);
     }
-
+    // sets modal to true which opens the modal
+    // messing around with this one
     function openModalReceipt() {
         setModalOpenReceipt(true);
     }
-
-    function closeModalReceipt() {
-        
+    // sets to false which closes the modal
+    function closeModalReceipt() {       
         setModalOpenReceipt(false);
     }
-
-    function openModalNewOrder() {
+    // same but for NewOrder
+    // messing with this one!!!!!
+    const openModalNewOrder = async(order) => {
+        // console.log(order);
+        setCurrentData(order);
         setModalOpenNewOrder(true);
     }
-
+    // setting modal to false
     function closeModalNewOrder() {
-        
         setModalOpenNewOrder(false);
     }
-
+    // This is what is shown to the user
     return (
+        // place all content in a container and centered it on the page
         <div className='ui container' style={{
             marginTop: '75px',
             textAlign: 'center',
             height: '75%',
+            // this sets the text for the entire page to default to 'whitesmoke'
             color: 'whitesmoke',
+            // font size for entire page. unless otherwise stated
             fontSize: '18px'
         }}>
-            
+            {/* link to allow returing to '/home' */}
             <div style={{ display: 'flex' }}>
                 <Link to="/home" className=''>
-                    -- Back
+                    <Icon name='long arrow alternate left'/>
+                    Back
                 </Link>
             </div>
-
+            {/* Information */}
             <h4 style={{ margin: '1px', color: 'whitesmoke' }}>Category Name:</h4>
-
+            {/* Lets you know which category you are in */}
             <h2 style={{ color: 'darkcyan',fontSize: '50px', marginTop: '1px' }}>
-                {category.categoryName} 
                 <Icon name='list alternate outline' color='blue' />
+                {category.categoryName}                
             </h2>
-
+            {/* further explains the purpose of app */}
             <p>Keep track of items in <span style={{ color:'yellow' }}>"{category.categoryName}"</span> by adding them with "Add Item"</p>
-
+            {/* logic for AddItem modal */}
             <AddItem isOpen={modalOpenAddItem} onClose={closeModalAddItem} />
-
+            
             <div className="ui container">
-                <div style={{}}>
-                    
+                {/* button for calling AddItem */}
+                <div style={{}}>                    
                     <Button color='blue' style={{ marginTop: '10px', marginBottom: '20px' }} onClick={openModalAddItem}>Add Item</Button>
                 </div>
+
                 <div>
+                {/* checks if there is items. if yes it returns the table */}
                 {items.length > 0 ? (
                 <Table color='teal' key={'teal'} celled inverted>
                     <TableHeader>
@@ -141,46 +159,47 @@ export default function Categories() {
                     <TableBody>
                         {items.map((item) => (
                             <TableRow key={item._id}>
+                                {/* name of item */}
                                 <TableCell>
                                     <Label color='blue' style={{ fontSize:'18px' }} ribbon>
+                                        {/* uppercases the first letter of every Item */}
                                         {item.itemName.charAt(0).toUpperCase() + item.itemName.slice(1)}
                                     </Label>
                                 </TableCell>
+                                {/* quantity of item */}
                                 <TableCell className="center aligned">
                                     {item.quantity}
                                 </TableCell>
+                                {/* price, if supplied, of item */}
                                 <TableCell className="right aligned">
                                     <Icon link name='dollar sign' style={{color:'black'}} />
                                     {item.price}
                                 </TableCell>
 
-                               
+                               {/* plus icon. will open NewOrder modal */}
                                 <TableCell>
-                                    <Icon link name='plus' style={{color:'green'}} />
+                                    <Icon link name='plus' style={{color:'green'}} onClick={() => openModalNewOrder(item)}/>
                                 </TableCell>
+                                {/* minus icon. will open Receipt modal */}
                                 <TableCell>
-                                    <Icon link name='minus' style={{color:'blue'}} />
+                                    <Icon link name='minus' style={{color:'blue'}} onClick={openModalReceipt}/>
                                 </TableCell>
-
-                                
-                                
+                                {/* will delete item when clicked */}
                                 <TableCell className="center aligned">
-                                    <Icon link name='delete' style={{color:'red'}} onClick={() => handleDelete(item._id)} />
-                                        
-                                        
-                                    
+                                    <Icon link name='delete' style={{color:'red'}} onClick={() => handleDelete(item._id)} />    
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
                 ):(
+                    // This is what will display if there aren't any items for the given category.
                     <p style={{ color:'yellow' }}>--Add items with the Add Item button--</p>
                 )}
                 </div>
             </div>
-            
-            <div style={{ marginTop: '40px' }}>
+            {/* commented out both buttons because of icons */}
+            {/* <div style={{ marginTop: '40px' }}>
             <p>Order allows you to add quantity to an existing item.</p>
             <button className='ui green button' onClick={openModalNewOrder}>Order</button>
             </div>
@@ -188,16 +207,21 @@ export default function Categories() {
             <div style={{ marginTop: '20px', marginBottom: '20px' }}>
                 <p>Receipt allows you to deduct quantity from an existing item.</p>
                 <button className='ui red button' onClick={openModalReceipt}>Receipt</button>
-            </div>
+            </div> */}
 
+            {/* This div provides a buffer for footer. Do not remove */}
             <div style={{ height: '50px' }}></div>
-
+            {/* NewOrder link */}
             <NewOrder
                 isOpen={modalOpenNewOrder}
                 onClose={closeModalNewOrder}
                 items={items}
+                name={currentData.itemName}
+                quantity={currentData.quantity}
+                price={currentData.price}
+                itemId={currentData._id}
             />
-
+            {/* Receipt link */}
             <Receipt
                 isOpen={modalOpenReceipt}
                 onClose={closeModalReceipt}
